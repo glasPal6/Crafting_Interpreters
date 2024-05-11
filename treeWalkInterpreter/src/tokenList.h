748db2a326a747d55c53d1fbed10672c91d5e3a9
glaspal6 <dylan.r.kamstra@gmail.com>
2024-05-11 15:37:36 +0200

    fix(list): can now push and pop items

--- treeWalkInterpreter/src/tokenList.h
+++ treeWalkInterpreter/src/tokenList.h
@@ -7,13 +7,13 @@
 #include <stdlib.h>
 
 typedef struct TokenList {
-    Token *token;
+    Token token;
     struct TokenList* next;
     int index;
 } TokenList;
 
-void listPush(TokenList* list, Token* token);
-void listPopEnd(TokenList* list);
+void listPush(TokenList** list, Token token);
+void listPopEnd(TokenList** list);
 void listPrint(TokenList* list);
 
 #endif // !TOKENLIST_H
@@ -26,58 +26,36 @@ void listPrint(TokenList* list);
 #ifdef TOKENLIST_IMPLEMENTATION
 #undef TOKENLIST_IMPLEMENTATION
 
-void listPush(TokenList* list, Token* token) {
-    TokenList* node = malloc(sizeof(TokenList));
-    node->token = token;
-    node->next = NULL;
+void listPush(TokenList** list, Token token)
+{
+    if (*list != NULL)
+        (*list)->index++;
 
-    if (list->token == NULL) {
-        printf("is null\n");
-        list = node; 
-        list->index = 0;
-    } else {
-        printf("is not null\n");
-        TokenList list_node = *list; 
-        int count = 0;
-        while (list_node.next!= NULL) { 
-            list_node = *list_node.next;
-            count += 1;
-        }
-        list_node.next = node;
-        list_node.index = count;
-    }
+    TokenList* node = (TokenList*)malloc(sizeof(TokenList));
+    node->token = token;
+    node->next = (*list);
+    node->index = 0;
+    
+    (*list) = node;
 }
 
-void listPopEnd(TokenList* list)
+void listPop(TokenList** list)
 {
-    if (list->token == NULL) {
+    if (*list == NULL) {
         printf("List is empty\n");
         return;
     }
-
-    TokenList* list_node = list;
-    while (list_node->next->next != NULL)
-            list_node = list_node->next;
-    
-    TokenList* last_node = list_node->next;
-    list_node->next = NULL;
-    free(last_node->token);
-    free(last_node);
+    TokenList* list_node = *list;
+    *list = (*list)->next;
+    free(list_node);
 }
 
 void listPrint(TokenList* list)
 {
-    if (list->token == NULL) {
-        printf("List is empty\n");
-        return;
+    while (list != NULL) {
+        printToken(list->token);
+        list = list->next;
     }
-
-    TokenList* list_node = list;
-    while (list_node != NULL) {
-        printToken(*list_node->token);
-        list_node = list_node->next;
-    }
-    free(list_node);
 }
 
 #endif //TOKENLIST_IMPLEMENTATION
